94272cbd1e5cf73124ed7fe885374d4d190e11cd
66rhythm <tanyabh2k1@gmail.com>
2021-05-18 23:57:55 +0530

    added

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -1,3 +1,54 @@
+// import 'package:flutter/material.dart';
+// import 'package:clima/utilities/constants.dart';
+
+// class CityScreen extends StatefulWidget {
+//   @override
+//   _CityScreenState createState() => _CityScreenState();
+// }
+
+// class _CityScreenState extends State<CityScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         decoration: BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage('images/city_background.jpg'),
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         constraints: BoxConstraints.expand(),
+//         child: SafeArea(
+//           child: Column(
+//             children: <Widget>[
+//               Align(
+//                 alignment: Alignment.topLeft,
+//                 child: FlatButton(
+//                   onPressed: () {},
+//                   child: Icon(
+//                     Icons.arrow_back_ios,
+//                     size: 50.0,
+//                   ),
+//                 ),
+//               ),
+//               Container(
+//                 padding: EdgeInsets.all(20.0),
+//                 child: null,
+//               ),
+//               FlatButton(
+//                 onPressed: () {},
+//                 child: Text(
+//                   'Get Weather',
+//                   style: kButtonTextStyle,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
@@ -7,6 +58,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +77,9 @@ class _CityScreenState extends State<CityScreen> {
               Align(
                 alignment: Alignment.topLeft,
                 child: FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                   child: Icon(
                     Icons.arrow_back_ios,
                     size: 50.0,
@@ -33,10 +88,20 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    cityName = value;
+                  },
+                ),
               ),
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
